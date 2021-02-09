432f6995ca31f82daf41ee7c9ee5a9f0f87ead60
Francesca Marinuzzi <france.marinuzzi0203@gmail.com>
2021-02-09 18:08:02 -0400

    fokin skills

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//SLIDER DEL HERO
 let i = 0;
 let img = [];
 let time = 4000;
@@ -21,5 +22,27 @@ function changeImg() {
 
 window.onload = changeImg;
 
+//JSON SKILLS
 
+let skills = '{ "skills" : [' +
+'{ "html":"90%"},' + '{"css":"60%" },' +
+'{ "javascript":"70%"},' + '{"python":"80%" },' +
+'{ "react":"70%"}]}';
+
+let obj = JSON.parse(skills);
+
+document.getElementById("stats0").innerHTML =
+'HTML:' + ' ' + obj.skills[0].html;
+
+document.getElementById("stats1").innerHTML =
+'CSS:' + ' ' + obj.skills[1].css;
+
+document.getElementById("stats2").innerHTML =
+'JavaScript:' + ' ' + obj.skills[2].javascript;
+
+document.getElementById("stats3").innerHTML =
+'Python:' + ' ' + obj.skills[3].python;
+
+document.getElementById("stats4").innerHTML =
+'React:' + ' ' + obj.skills[4].react;
 
