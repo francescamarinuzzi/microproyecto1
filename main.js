fef52a7103374f209bd9dde665ed29c2edabddfb
Francesca Marinuzzi <france.marinuzzi0203@gmail.com>
2021-02-09 15:32:45 -0400

    hero y slider2

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-# microproyecto1
+let i = 0;
+let img = [];
+let time = 4000;
+
+img[0] = 'img/IMG_1290.JPG';
+img[1] = 'img/IMG_1567.JPG';
+img[2] = 'img/IMG_2379.JPG';
+
+
+function changeImg() {
+    document.slider.src = img[i];
+
+    if(i < img.length - 1){
+        i++;
+    } else {
+        i = 0;
+    }
+
+    setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
+
+
+
